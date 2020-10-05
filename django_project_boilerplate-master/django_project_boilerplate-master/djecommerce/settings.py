fde43bebd1ff49e6c71a066ed9c20b038ba8c6e5
Zarak shahjee <47606946+Mohammad-zarak-shahjee@users.noreply.github.com>
2020-10-05 15:45:39 +0530

    heroku dependencies commit

--- django_project_boilerplate-master/django_project_boilerplate-master/djecommerce/settings.py
+++ django_project_boilerplate-master/django_project_boilerplate-master/djecommerce/settings.py
@@ -3,10 +3,10 @@ from decouple import config
 
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
-DEBUG = True
+DEBUG = False
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','yander.herokuapp.com']
 SECRET_KEY = config('SECRET_KEY')
 
 INSTALLED_APPS = [
